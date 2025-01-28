05df79e6ac5a8c840fcfb9d61857a03a5a64f9be
erfjab <erfjab@gmail.com>
2025-01-29 02:55:59 +0330

    feat: add export config reader

---
+++ app/settings/config/_env.py
@@ -0,0 +1,11 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class EnvSettingsFile(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file="export.env", case_sensitive=True, extra="ignore"
+    )
+
+    EXPORT_HOST: str = ""
+    EXPORT_USERNAME: str = ""
+    EXPORT_PASSWORD: str = ""
